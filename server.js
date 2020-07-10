0cc417acc9ea73310a96ffb36232032fbba0565c
Shezray Husain <64660355+shezray@users.noreply.github.com>
2020-07-10 01:42:09 -0500

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-colorful-0434',
     user : 'shezrayhusain',
     password : '',
     database : 'facescanner'
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is working!') })
+app.get('/', (req, res)=> { res.send('it is working!!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
